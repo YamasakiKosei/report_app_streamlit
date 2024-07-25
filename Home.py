14ac36ec4f0300a26d43a799acca307bec28c5e5
Kosei Yamasaki <YamasakiKosei@icloud.com>
2024-07-25 04:29:23 +0000

    1.0

---
+++ Home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from PIL import Image
+
+st.title('CheckMake')
+st.write('点検報告書作成アプリ')
+st.caption('Update：2024/07')
+st.divider()
+
+st.header('Info')
+st.write('医療機器の点検報告書を作成し、Excel形式で保存することができます')
+image = Image.open('data/点検報告書 例.png')
+st.image(image, width=400)
+st.divider()
+
+st.header('Pages')
+st.page_link('pages/輸液ポンプ.py', label='輸液ポンプ')
